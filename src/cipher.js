0e07cfda292e0235cc9a94c144b247200a9b87aa
UnaTal <taliagyg@gmail.com>
2019-01-17 21:00:22 -0600

    ya con función encode

--- src/cipher.js
+++ src/cipher.js
@@ -1,20 +1,19 @@
 window.cipher = {
 
   encode: (offsetB, messageB) => {
-    let offsetC = parseInt(offsetB)
+    let offsetC = parseInt(offsetB);
     let encodeC = '';
     for ( let i = 0; i < messageB.length; i++) {
-      const ascii = messageB[i].chartCodeAt();
-      //console.log(messageB[i])
-      //probar con let
+      const ascii = messageB[i].charCodeAt();
       const formula = (ascii - 65 + offsetC) % 26 + 65;
       const encodeMessage = String.fromCharCode(formula);
       encodeC += encodeMessage
       }
+      //console.log(encodeC)
       return encodeC;
   }
-  console.log(encodeC)
-  document.getElementById('nombre').innerHTML `Tu mensaje encriptado es ${encodeMessage}`    }
+  //console.log(encodeC);
+  //document.getElementById('encode').innerHTML `Tu mensaje encriptado es ${encodeMessage}`;
 
 
   //decode: () => {
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@
 const message = document.getElementById('message');
 const offset = document.getElementById('offset');
 const encodeButton = document.getElementById('encodeButton');
-console.log(encodeButton)
+//console.log(encodeButton)
 //agrega evento al botón e indica que hace referencia a la función encode
 encodeButton.addEventListener('click', () => {
     const messageB = message.value.toUpperCase();
     const offsetB = offset.value;
+    
     window.cipher.encode(offsetB, messageB);
  });
     
